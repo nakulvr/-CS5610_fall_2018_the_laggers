d44fc41fc22c232813a02289abfb2184ca318a7e
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-05 16:32:21 -0500

    Added User Service methods

--- src/app/Services/user.service.client.ts
+++ src/app/Services/user.service.client.ts
@@ -25,4 +25,45 @@ export class UserServiceClient {
     //   }
     // }).then(response => response.json());
   }
+  findUsername(username) {
+    const credentials = {
+      username: username,
+    };
+    return null;
+    // return fetch('url', {
+    //   method: 'post',
+    //   body: this.JSON.stringify(credentials),
+    //   credentials: 'include',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   }
+    // }).then(response => response.json());
+  }
+
+  createUser(username, password) {
+    const user = {
+      username: username,
+      password: password
+    };
+
+    return {
+      username: 'hari',
+      password: '123',
+      Firstname: '',
+      lastname: '',
+      email: '',
+      followers: [],
+      following: [],
+      comments: []
+    };
+    // return fetch('https://secure-coast-10881.herokuapp.com/api/user', {
+    //   body: JSON.stringify(user),
+    //   credentials: 'include', // include, same-origin, *omit
+    //   method: 'post',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   }
+    // });
+  }
+
 }
