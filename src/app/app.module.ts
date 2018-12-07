a7c1d8c064633cb3f3108f72c60657a6d759b11a
akashparikh <parikh.ak@husky.neu.edu>
2018-12-06 20:01:22 -0500

    Added movies page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewtvComponent } from './newtv/newtv.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { UsersComponent } from './users/users.component';
 import { FeedComponent } from './feed/feed.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { FeedComponent } from './feed/feed.component';
     NewtvComponent,
     DiscoverComponent,
     UsersComponent,
-    FeedComponent
+    FeedComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,8 +7,10 @@ import {NewtvComponent} from './newtv/newtv.component';
 import {DiscoverComponent} from './discover/discover.component';
 import {UsersComponent} from './users/users.component';
 import {FeedComponent} from './feed/feed.component';
+import {MoviesComponent} from './movies/movies.component';
 
 const appRoutes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'profile', component: ProfileComponent},
@@ -17,6 +19,8 @@ const appRoutes: Routes = [
   {path: 'discover', component: DiscoverComponent},
   {path: 'users', component: UsersComponent},
   {path: 'feed', component: FeedComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: '**', component: HomeComponent},
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
