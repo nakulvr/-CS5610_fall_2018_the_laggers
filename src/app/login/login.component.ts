33e36effa33a5656ceef2ac2234eead560e2d8d8
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-08 11:56:30 -0500

    refactored login component to handle api response

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 import {UserServiceClient} from '../services/user.service.client';
+
 
 @Component({
   selector: 'app-login',
@@ -11,27 +13,42 @@ export class LoginComponent implements OnInit {
   password = '' ;
   service;
   curr_user;
+  router;
 
-  constructor(private userservice: UserServiceClient) {
+  constructor(private userservice: UserServiceClient, private router: Router) {
     this.service = userservice;
+    this.router = router;
   }
 
   login(username, password) {
     this.username = username;
     this.password = password;
-    let res = this.service.login(username, password);
-    this.loginValidation(res);
+    this.service.login(username, password).then(user =>{
+      console.log("after output");
+      console.log(user);
+      if (user !== null) {
+        this.service.setUser(user);
+        this.router.navigate(['home']);
+      }
+      else{
+        alert('Invalid User Credentials. Please try again or Register yourself!!!');
+      }
+    });
+    // this.loginValidation(res);
     // this.service.login(username, password)
     //   .then(user => this.curr_user = user)
     //   .then(() => this.loginValidation(this.curr_user));
   }
 
   loginValidation(user) {
+    console.log("after output");
+    console.log(user);
     if (user !== null) {
-      alert('got to profile page');
+      this.router.navigate(['home']);
     }
     else{
-      alert('wrong credentials');}
+      alert('Invalid User Credentials. Please try again or Register yourself!!!');
+    }
   }
 
   ngOnInit() {
