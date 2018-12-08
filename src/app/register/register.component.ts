140cffdcef6520e3dead88d386a998c0c6bb25fa
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-08 11:57:42 -0500

    refactored register page to handle api response

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 import {UserServiceClient} from '../services/user.service.client';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
  password = '';
  verifyPassword = '';
  userservice;
-  constructor(private service: UserServiceClient) {
+  constructor(private service: UserServiceClient, private router: Router) {
     this.userservice = service;
   }
 
@@ -24,17 +25,28 @@ export class RegisterComponent implements OnInit {
       alert('Password does not match. Please try again!!!');
       return;
     }
-    let response = this.userservice.findUsername(username);
-    if (response !== null) {
-      alert("Username already exists");
-    }
-    else {
-      let result = this.userservice.createUser(username, password);
-      if (result !== null)
-        alert('Go to Profile Page');
-      else
-        alert('Registration failed due to server error. Please try again');
-    }
+
+    this.userservice.findUserByUsername(username).then((uname) => {
+      console.log("success");
+      console.log(uname);
+      if (uname === null || (uname.constructor === Array && uname.length === 0){
+        this.userservice.createUser(username, password).then(() => this.router.navigate(['login']));
+      }
+      else {
+        alert('UserName already exists. Please try a new UserName');
+      }
+    });
+    // const response = this.userservice.findUsername(username);
+    // if (response !== null) {
+    //   alert('Username already exists');
+    // }
+    // else {
+    //   const result = this.userservice.createUser(username, password);
+    //   if (result !== null)
+    //     alert('Go to Profile Page');
+    //   else
+    //     alert('Registration failed due to server error. Please try again');
+    // }
   }
 
   ngOnInit() {
